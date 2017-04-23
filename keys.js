e66236653608451caceabdf36a9be525c6bcfcb1
Ian-Maynard <maynard.ian@gmail.com>
2017-04-23 13:41:10 -0400

    Setup, plus case structure framework

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'mnd6dvUrPINZ6x8EN9mU6zjtN>',
+  consumer_secret: 'QC7w5a2iKB8OFehwD64Iiz4ycJ10b994bQfIPJ87HEhaxz969p',
+  access_token_key: '856093098648141825-L3crJlTp7cQAfsF42Gjt86jJRSHHdqP',
+  access_token_secret: 'PWo7MGgC0rypLTjoQbeGXj8GhmYMD0ukOIuCvrH0tEpXp',
+}
---
+++ liri.js
@@ -0,0 +1,82 @@
+
+// Combines two command line arguments (strings).
+// console.log(process.argv[2] + process.argv[3]);
+
+// Adds two command line arguments (numbers).
+// console.log(parseFloat(process.argv[2]) + parseFloat(process.argv[3]));
+// Liri Homework 
+
+var = require("fs");
+
+
+
+
+
+switch(process.argv[3]) {
+
+        case 'my-tweets':
+            ;
+       
+        break;
+
+        case 'spotify-this-song':
+        
+        break;
+
+        case 'movie-this':
+        
+        break;
+
+		case 'do-what-it-says':
+       
+        break;
+
+        default:
+        console.log('I did not understand that');
+        break;
+}
+
+
+
+function logCmd()
+{
+
+
+} // Log commmands
+
+
+
+function twitter(){
+
+logCmd();
+}// Twitter read/write
+
+
+function movie()
+{
+
+logCmd();
+} // Movies
+
+
+function doWhat()
+{
+
+logCmd();
+} // Do what I say 
+
+
+function spotify()
+{
+
+logToTxt();
+} // Spotify read/write
+
+
+
+
+
+
+
+
+
